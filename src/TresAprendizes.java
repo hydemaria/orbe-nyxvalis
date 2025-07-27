b5a64fff95c09342bfa3eda373fbe1ab01eaca33
hydemaria <paganimarialuiza.s@gmail.com>
2025-07-27 15:53:04 -0300

    Primeiro commit: adicionando os arquivos iniciais

---
+++ src/TresAprendizes.java
@@ -0,0 +1,79 @@
+/* Crie 3 aprendizes (ex: Luna, Elio, Solene), com dados diferentes.
+Declare idade e altura de cada um. Imprima a ficha deles separadinha com ---. */
+
+
+public class TresAprendizes {
+    public static void main(String[] args) {
+        String bruxa1 = "Luna";
+        int idadeBruxa1 = 19;
+        short nivelPocao = 12;
+        double forcaEspiritual1 = 45.1;
+        char elementoBruxa1 = 'A';
+        double alturaBruxa1 = 1.65;
+
+        System.out.println("Ficha Mistica da Bruxa Luna");
+        System.out.println("Nome: " + bruxa1);
+        System.out.println("Idade: " + idadeBruxa1);
+        System.out.println("Altura: " + alturaBruxa1);
+        System.out.println("Nivel de Poção da Bruxa " + nivelPocao);
+        System.out.println("O nível espiritual é: " + forcaEspiritual1);
+        System.out.println("A inicial do elemento da bruxa é: " + elementoBruxa1);
+
+        System.out.println("----------------------");
+
+        String bruxa2 = "Elio";
+        int idadeBruxa2 = 35;
+        double alturaBruxa2 = 1.85;
+        double forcaEspiritual2 = 61.8;
+        char elementoBruxa2 = 'F';
+        short nivelPocao2 = 23;
+
+        System.out.println("Ficha Mística da Bruxa Elio");
+        System.out.println("Nome: " + bruxa2);
+        System.out.println("Altura: " + alturaBruxa2);
+        System.out.println("Idade: " + idadeBruxa2);
+        System.out.println("Nivel de Poção da Bruxa Elio: " + nivelPocao2);
+        System.out.println("O nível espiritual é: " + forcaEspiritual2);
+        System.out.println("A inicial do elemento da bruxa é: " + elementoBruxa2);
+
+        System.out.println("----------------------");
+
+        String bruxa3 = "Solene";
+        int idadeBruxa3 = 89;
+        double alturaBruxa3 = 1.55;
+        char elementoBruxa3 = 'A';
+        double forcaEspiritual3 = 95.8;
+        short nivelPocao3 = 100;
+
+
+        System.out.println("Ficha Mística da Bruxa Solene");
+        System.out.println("Nome: " + bruxa3);
+        System.out.println("Altura: " + alturaBruxa3);
+        System.out.println("Idade: " + idadeBruxa3);
+        System.out.println("Nivel de Poção da Bruxa Solene: " + nivelPocao3);
+        System.out.println("O nível espiritual é: " + forcaEspiritual3);
+        System.out.println("A inicial do elemento da bruxa é: " + elementoBruxa3);
+
+        System.out.println("----------------------");
+
+
+
+    }
+}
+
+
+/*      String nome = "Hyde Pronaia";
+        int idade = 28;
+        double altura = 1.58;
+        char inicial = 'H';
+        boolean iniciadaNoCoven = true;
+        long saldoDeEnergia = 99999999999999L;
+
+        System.out.println("Ficha Mistica da Bruxa");
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Letra inicial: " + inicial);
+        System.out.println("Iniciada no coven: " + iniciadaNoCoven);
+        System.out.println("Saldo de Energia Mística: " + saldoDeEnergia);
+        */
